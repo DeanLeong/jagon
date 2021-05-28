6413ebdb1c3a8365469789665d19b72d1496e7a3
DeanLeong <dean.g.leong@gmail.com>
2021-05-28 10:43:10 -0400

    npc controller created

---
+++ app/controllers/npc_controller.rb
@@ -0,0 +1,51 @@
+class NpcController < ApplicationController
+
+  #Get /characters
+  def index
+      @npcs = Npc.all
+
+     render json: @characters
+  end
+
+  #Get /characters/1
+  def show
+    render json: @npc
+  end
+
+  def create
+   @npc = Npc.new(npc_params)
+
+   if character.save
+     render json: @character, status: created
+   else
+     render json: @character.errors, status: :unprocessable_entity
+   end
+  end
+
+  def update 
+   @npc = Npc.find params[:id]
+
+   if @npc.update(npc_params)
+     render json: @npc
+   else
+     render json: @npc.errors, status: :unprocessable_entity
+   end
+  end
+
+  def destroy
+    @npc = Character.find params[:id]
+
+   @character.destroy
+  end
+
+  private
+
+  def set_npc
+    @npc = Npc.find(params[:id])
+  end
+
+  def npc_params
+   params.require(:character).permit(:name, :imgURL, :biography, :notes)
+  end
+end
+
